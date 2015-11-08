d9463c3d8c3641ec8916d2f0f32ed6cf2f1fa778
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-11-07 18:46:33 -0600

    Tags pages.

--- _plugins/tag_gen.rb
+++ _plugins/tag_gen.rb
@@ -1,6 +1,6 @@
 module Jekyll
 
-  class TagIndex < Page    
+  class TagPage < Page    
     def initialize(site, base, dir, tag)
       @site = site
       @base = base
@@ -8,9 +8,24 @@ module Jekyll
       @name = 'index.html'
 
       self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
+      self.read_yaml(File.join(base, '_layouts'), 'tag_page.html')
       self.data['tag'] = tag
       self.data['title'] = tag
+    end
+  end
+  
+  class TagIndex < Page
+    def initialize(site, base, dir, tags)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+      
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
+      self.data['tags'] = tags
+      
+      
     end
   end
 
@@ -18,16 +33,26 @@ module Jekyll
     safe true
     
     def generate(site)
-      if site.layouts.key? 'tag_index'
+      if site.layouts.key? 'tag_page'
         dir = 'tag'
+        
+        write_tag_index(site, dir, site.tags.keys)
+        
         site.tags.keys.each do |tag|
-          write_tag_index(site, File.join(dir, tag), tag)
+          write_tag_page(site, File.join(dir, tag), tag)
         end
       end
     end
   
-    def write_tag_index(site, dir, tag)
-      index = TagIndex.new(site, site.source, dir, tag)
+    def write_tag_index(site, dir, tags)
+      index = TagIndex.new(site, site.source, dir, tags)
+      index.render(site.layouts, site.site_payload)
+      index.write(site.dest)
+      site.pages << index
+    end
+  
+    def write_tag_page(site, dir, tag)
+      index = TagPage.new(site, site.source, dir, tag)
       index.render(site.layouts, site.site_payload)
       index.write(site.dest)
       site.pages << index
