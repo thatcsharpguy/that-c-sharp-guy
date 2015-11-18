546ee8f13c7152bd38ed1d03f155eb76b5969f65
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-11-18 14:57:00 -0600

    Delete PXM

--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,5 @@
 jekyll build
 get-childitem .\_site -include *.svg -recurse | foreach ($_) {remove-item $_.fullname}
+get-childitem .\_site -include *.pxm -recurse | foreach ($_) {remove-item $_.fullname}
+get-childitem .\_site -include *.sketch -recurse | foreach ($_) {remove-item $_.fullname}
+
--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
 jekyll build
 find _site -name '*.svg' -delete
-find _site -name '*.sketch’ -delete
+find _site -name '*.sketch' -delete
+find _site -name '*.pxm' -delete
